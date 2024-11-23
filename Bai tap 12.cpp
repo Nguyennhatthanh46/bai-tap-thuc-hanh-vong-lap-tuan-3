449ee1fc08a0fbf5d840d63d1b57d22d41b081a1
Nguyen Nhat Thanh <nthanh100406@gmail.com>
2024-11-23 23:03:20 +0700

    Tìm ước chung lớn nhất

--- Bai tap 12.cpp
+++ Bai tap 12.cpp
@@ -1,3 +1,17 @@
+/*
+TÌM ƯỚC CHUNG LỚN NHẤT 
+
+Cho 2 số nguyên dương a, b (1 ≤ a,b ≤ 2.10^9). Tìm ước chung lớn nhất của 2 số đó.
+INPUT                  OUTPUT
+----------------------------------
+3     9                3
+__________________________________
+18    12               6
+__________________________________
+5     7                1
+
+*/
+
 #include <iostream>
 using namespace std;
 int main(){
