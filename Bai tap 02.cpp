b3b1168962b58a1e86f0ed2bc64931da54b2154a
Nguyen Nhat Thanh <nthanh100406@gmail.com>
2024-11-24 01:19:48 +0700

    In hinh v2

---
+++ Bai tap 02.cpp
@@ -0,0 +1,35 @@
+/*
+IN HÌNH v2
+Viết chương trình nhập số nguyên dương n. In ra màn hình theo định dạng bên dưới
+
+INPUT                            OUTPUT
+---------------------------------------------
+3	                                  #
+                                     ##
+                                    ###
+---------------------------------------------
+5	                                  #
+                                     ##
+                                    ###
+                                   ####
+                                  #####
+*/    
+
+#include <iostream>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+for(int i = 1; i <= n; i++){
+    for(int j = 1; j <= n;j++){
+        if(j<=(n-i)){
+        cout<<" ";
+        }else{
+            cout<<"#";
+        }
+    }
+    cout<<endl;
+}
+return 0;
+}
