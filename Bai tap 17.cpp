f90dc4804052c5cf0b2990d960a6f93bc40c327e
Nguyen Nhat Thanh <nthanh100406@gmail.com>
2024-11-23 22:23:11 +0700

    Tổng lớn nhất

--- Bai tap 17.cpp
+++ Bai tap 17.cpp
@@ -1,3 +1,25 @@
+/*
+Cho dãy số nguyên a1, a2, . . ., an. 
+Trong trường hợp cần thiết được phép đổi chổ nhiều nhất 1 lần 2 số ai và aj, i ≠ j, 1 ≤ i, j ≤ n.
+
+Tổng s được xác định theo công thức: s = a1–a2+a3–a4+ . . . +(-1)n-1an.
+
+Hãy xác định tổng lớn nhất có thể đạt được.
+
+Dữ liệu: Vào từ thiết bị nhập chuẩn:
+- Dòng đầu tiên chứa một số nguyên n (2 ≤ n ≤ 10^5),
+- Dòng thứ 2 chứa n số nguyên a1, a2, . . ., an (1 ≤ ai ≤ 1000, i = 1 ÷ n).
+Kết quả: Đưa ra thiết bị xuất chuẩn một số nguyên – tổng lớn nhất tìm được.
+
+INPUT                  OUTPUT
+---------------------------------
+2
+                        1
+1 2  
+
+Ví dụ:
+*/
+
 #include <iostream>
 using namespace std;
 int main() { 
