dacc947b463a28cf20527eb2a0e81928aac1faaa
Nguyen Nhat Thanh <nthanh100406@gmail.com>
2024-11-23 23:00:19 +0700

    Tìm ước chung lớn nhất (Giới hạn thời gian)

--- Bai tap 13.cpp
+++ Bai tap 13.cpp
@@ -1,5 +1,18 @@
-#include <iostream>
+/*
+TÌM ƯỚC CHUNG LỚN NHẤT (Giới Hạn Thời Gian)
 
+Cho 2 số nguyên dương a, b (1 ≤ a,b ≤ 2.10^9). Tìm ước chung lớn nhất của 2 số đó.
+INPUT                  OUTPUT
+----------------------------------
+3     9                3
+__________________________________
+18    12               6
+__________________________________
+5     7                1
+
+*/
+
+#include <iostream>
 using namespace std;
 int main() {
 int a, b;
